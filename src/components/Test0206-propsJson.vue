a45764eaecb7f83aac5ac559c42a6e80ebee4191
xpluso <xxxplusooo913@gmail.com>
2025-02-06 16:53:32 +0800

    vue+API (reactive版)

--- src/components/Test0206-propsJson.vue
+++ src/components/Test0206-propsJson.vue
@@ -1,13 +1,13 @@
 <template>
-    <p v-if="random">{{ random }}</p>
+    <p v-if="random">{{ random.id }}{{ random.name }}</p>
 </template>
 
 <script setup lang="ts">
 
 // API
-import { ref, onMounted } from "vue";
+import { ref, reactive, onMounted } from "vue";
 const users = ref([]);//陣列，用來裝API的值
-const random = ref('');//只裝random要的
+const random = reactive([{ id: 0, name: '' }]);//只裝random要的
 
 onMounted(async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -15,7 +15,8 @@ onMounted(async () => {
 
     if (users.value.length > 0) {
         const randomIndex = Math.floor(Math.random() * users.value.length);
-        random.value = users.value[randomIndex].id + users.value[randomIndex].name;
+        random.id = users.value[randomIndex].id;
+        random.name = users.value[randomIndex].name;
     }
 });
 </script>
