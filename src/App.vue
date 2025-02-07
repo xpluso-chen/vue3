b1fb4e6b08c55ecabee0b2ff59dc4a97149ff569
xpluso <xxxplusooo913@gmail.com>
2025-02-07 15:48:43 +0800

    新增SCSS

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   
-    <Test class="logo"/>
+    <Test class="logo">
+</Test>
 
-  
+13
+<Test2 class="logo"/>
 </template>
 
 <script setup lang="ts">
 import Test from './components/Test0207-api.vue'
+import Test2 from './components/Test0203-ref.vue'
 
 
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@use "sass:map";
 .logo {
     border-radius: 50%;
     background-color: rgb(211, 145, 57);
     width: 300px;
     height: 300px;
+    color: map.get($color, nav-deepblue );
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
+import './assets/styles/main.scss' // 直接載入 SCSS，Vite 會自動轉換
 
 createApp(App).mount('#app')
